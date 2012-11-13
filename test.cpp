0eaed450e6b09185b483fc529365d52e14cb96cc
Daming <daminglu2012@hotmail.com>
2012-11-13 15:00:09 -0800

    edit in branch

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main ()
 {
+  // what if add something here, in a branch of a forked repo ???
   cout << "Hello Confliction!";
   return 0; // this is forked in the forked repo
 }
